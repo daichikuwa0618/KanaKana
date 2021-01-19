a1b0a93d7426fd81b0e88910e1f7d2e2c0f3a6c1
Daichi Hayashi <daichikuwa0618@gmail.com>
2021-01-20 01:29:55 +0900

    add: convertViewModel の I/F を定義

---
+++ KanaKana/View/Convert/ConvertViewModelProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  ConvertViewModelProtocol.swift
+//  KanaKana
+//
+//  Created by Daichi Hayashi on 2021/01/20.
+//
+
+import Foundation
+
+protocol ConvertViewModelProtocol {
+    /// ひらがなに変換する
+    func convert(_ kanji: String)
+}
